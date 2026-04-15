1f983434f07d47653c45551860a823f6aea0715f
Legend-of-Tony <antonyangoco@gmail.com>
2026-04-15 08:27:10 -0700

    Added some details about the wedidng and groom's contact info

--- client/src/features/map/Map.tsx
+++ client/src/features/map/Map.tsx
@@ -1,20 +1,41 @@
-import GridLayout from "../../components/layout/GridLayout"
+import GridLayout from "../../components/layout/GridLayout";
 
 const Map = () => {
   return (
-    <GridLayout id='map' className='w-full bg-primary lg:h-screen lg:grid-rows-9'>
-        <div id='map-container' className='order-2 lg:order-1 lg:col-start-1 lg:col-span-4 lg:row-start-2 lg:row-span-7  '>
-            <iframe  className=" w-full h-[50vh] lg:h-full border:0 shadow-xl" loading="lazy" allowFullScreen src="https://www.google.com/maps/embed/v1/place?q=place_id:EigxOTE1IFcgR3VhZGFsdXBlIFJkLCBNZXNhLCBBWiA4NTIwMiwgVVNBIjESLwoUChIJqfhITpcHK4cRiP13HDBPGGUQ-w4qFAoSCZc4mjopqCuHEaQRLxUItSYs&key=AIzaSyAR7XiEpmSnVko_4a9sP2XS-BmVsEBHvGo"></iframe> 
-        </div>
-        <div id='details-container' className=" order-1 lg:order-2 lg:col-start-5 lg:col-span-3 lg:row-start-2 lg:row-span-7 text-white lg:px-4 px-8 lg:pt-0 pt-10  flex flex-col gap-4 h-[50vh]">
-            <h2 className="text-5xl lg:text-7xl font-bold">Event Details</h2>
-            <hr />
-            <p className="text-xl lg:text-3xl font-light">When & Where</p>
-            <hr />
-            <p className=" font-extralight lg:font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
+    <GridLayout
+      id="map"
+      className="w-full bg-primary lg:h-screen lg:grid-rows-9"
+    >
+      <div
+        id="map-container"
+        className="order-2 lg:order-1 lg:col-start-1 lg:col-span-4 lg:row-start-2 lg:row-span-7  "
+      >
+        <iframe
+          className=" w-full h-[50vh] lg:h-full border:0 shadow-xl"
+          loading="lazy"
+          allowFullScreen
+          src="https://www.google.com/maps/embed/v1/place?q=place_id:EigxOTE1IFcgR3VhZGFsdXBlIFJkLCBNZXNhLCBBWiA4NTIwMiwgVVNBIjESLwoUChIJqfhITpcHK4cRiP13HDBPGGUQ-w4qFAoSCZc4mjopqCuHEaQRLxUItSYs&key=AIzaSyAR7XiEpmSnVko_4a9sP2XS-BmVsEBHvGo"
+        ></iframe>
+      </div>
+      <div
+        id="details-container"
+        className=" order-1 lg:order-2 lg:col-start-5 lg:col-span-3 lg:row-start-2 lg:row-span-7 text-white lg:px-4 px-8 lg:pt-0 pt-10  flex flex-col gap-4 h-[50vh]"
+      >
+        <h2 className="text-5xl lg:text-7xl font-bold">COME AND JOIN US</h2>
+        <hr />
+        <p className="text-xl lg:text-3xl font-light">
+          Villa Tuscana Reception Hall
+        </p>
+        <hr />
+        <ul className=" font-extralight lg:font-light list-disc">
+          <li>Saturday, October 24th</li>
+          <li>4PM - 12AM</li>
+          <li>Black Tie Event</li>
+          <li>Contact Lucas with any questions: (602) 573-5553</li>
+        </ul>
+      </div>
     </GridLayout>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
